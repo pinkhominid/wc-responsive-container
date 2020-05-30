6d142bca1fabd5f743f81f660ae0abb5dcba8c3e
pinkhominid <pinkhominid@birdbomb.com>
2020-05-30 11:02:19 -0400

    Factor out core behavior as ResponsiveElementMixin

--- wc-responsive-container.js
+++ wc-responsive-container.js
@@ -7,7 +7,7 @@
  * https://philipwalton.com/articles/responsive-components-a-solution-to-the-container-queries-problem/
  */
 
-// Create a single observer for all <wc-responsive-container> elements.
+// Create a single observer for all responsive elements
 const ro = new ResizeObserver(entries => {
     var defaultBreakpoints = {sm: 384, md: 576, lg: 768, xl: 960};
 
@@ -30,13 +30,25 @@ const ro = new ResizeObserver(entries => {
     });
 });
 
-class ResponsiveContainerElement extends HTMLElement {
+// Add responsive behavior to any element
+export const ResponsiveElementMixin = (superclass) =>
+  class extends superclass {
+    connectedCallback() {
+      if(super.connectedCallback) super.connectedCallback();
+      ro.observe(this);
+    }
+    disconnectedCallback() {
+      if(super.disconnectedCallback) super.disconnectedCallback();
+      ro.unobserve(this);
+    }
+  };
+
+export class ResponsiveContainerElement extends ResponsiveElementMixin(HTMLElement) {
   connectedCallback() {
+    super.connectedCallback();
+    // Default to display block if inline
+    // TODO: find a low specificity way w/o extra friction (shadow DOM)
     if (this.style.display.startsWith('inline')) this.style.display = 'block';
-    ro.observe(this);
-  }
-  disconnectedCallback() {
-    ro.unobserve(this);
   }
 }
 
